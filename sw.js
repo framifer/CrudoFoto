acfd1e12b0588985d9e0572802439606f1dadb76
framifer <framifer87@gmail.com>
2026-09-25 07:56:19 +0200

    Change CACHE_NAME to 'crudofoto-v3'
    Updated cache name to version 3 for cache invalidation.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Al primo caricamento online mette in cache i file; poi l'app funziona
 // anche senza rete. Aggiorna il numero di versione per invalidare la cache.
 
-const CACHE_NAME = 'crudofoto-v2';
+const CACHE_NAME = 'crudofoto-v3';
 
 // File che compongono il "guscio" dell'app da rendere disponibili offline.
 const APP_SHELL = [
